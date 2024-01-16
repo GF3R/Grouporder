85785083ee42df44cca098bcd9a9fc79be60b674
Tenzin Nyidon Sangatsang <tenzinnyidon.sangatsang@students.bfh.ch>
2024-01-16 15:18:28 +0100

    Update: forgot to delete

--- src/model/GroupOrder.ts
+++ src/model/GroupOrder.ts
@@ -8,51 +8,16 @@ export class GroupOrder {
   total?: number;
 
   constructor(name: string) {
-    // this.id = id;
     this.name = name;
-    // this.orders = orders;
-    // this.total = total;
-    // this.total = orders.reduce((acc, order) => acc + order.total, 0);
   }
 
   createFormGroup(formBuilder: FormBuilder): FormGroup<{
     name: FormControl;
-    // orders: FormControl;
-    // total: FormControl;
   }> {
     return formBuilder.group({
       name: [this.name],
-      // orders: [this.orders],
-      // total: [this.total],
     });
   }
-
-  // static createFromForm(
-  //   id: string,
-  //   form: FormGroup<{
-  //     name: FormControl;
-  //     orders: FormControl;
-  //     total: FormControl;
-  //   }>,
-  //   orderItems: string[]
-  // ): GroupOrder {
-  //   const orders: Order[] | null | undefined = form.value.orders;
-
-  //   const orderList: Order[] = orders ? orders.map((order: any) => {
-  //     return new Order(
-  //       order.id,
-  //       order.customerName,
-  //       order.items,
-  //       order.total
-  //     );
-  //   }) : [];
-
-  //   return new GroupOrder(
-  //     id,
-  //     form.value.name,
-  //     orderList,
-  //     +form.value.total
-  //   );
 
   static createFromForm(
     form: FormGroup<{
@@ -63,7 +28,6 @@ export class GroupOrder {
 
       form.value.name
     );
-
   }
 
 }
